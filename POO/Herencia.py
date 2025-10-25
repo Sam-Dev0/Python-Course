d7e8e80d01efb9368a9c8e39dc4f7bd42b2bd12d
Samuel Molina <138609350+Sam-Dev0@users.noreply.github.com>
2025-10-25 11:02:54 -0500

    Enhance Herencia.py with multiple inheritance examples
    Added examples of multiple inheritance and usage of super() in the Herencia.py file.

--- POO/Herencia.py
+++ POO/Herencia.py
@@ -1,3 +1,8 @@
+"""
+Herencia: es cuando una clase (Padre) le hereda las caracteristicas y habilidades a una clase (Hija),
+la (Hija) puede agregar y cambiar cosas
+"""
+#Clase herencia
 class Padre:
     def __init__(self,nombre,energia):
         self.nombre = nombre
@@ -30,3 +35,40 @@ k10 = perroGuia('Gustavo',100)
 k10.ladrar()
 k10.trabajar()
 k10.ayudar()
+
+"""
+Herencia multiple: una clase hereda de varios padres: abuelo -> padre -> hijo...
+MRO(orden de metodo de resolucion): Si A no tiene el método, python lo busca en B; si no está, va a C, 
+y así sigue por la cadena de ancestros ya calculada en el __mro__.
+Super():nos permite acceder a los métodos de la clase padre desde una de sus hijas.
+"""
+#Clase herencia multiple
+class PerrodeShow(Perro):
+    def presentarse(self):
+        print('Soy un perro que hace trucos')
+
+class RastreadorMixin():#Mixin bloque reutilizable
+    def rastrer(self):
+        print('Estoy rastreando')
+
+class PerroRastreadordeShow(PerrodeShow, RastreadorMixin): #Hereda de los que estan por encima de ella
+    pass
+tony = PerroRastreadordeShow('Tony', 10000)
+tony
+tony.presentarse()
+tony.rastrer()
+
+#Clase usando super()
+class Perro(Animal):
+    def __init__(self,especie,edad,dueño)
+        #Alternativa 1
+        self.especie = especie
+        self.edad = edad
+        self.dueño = dueño
+        #Alternativa 2
+        super().__init__(especie,edad)
+        self.dueño = dueño
+mi_perro = Perro('Mamifero', 7, 'Luis')
+mi_Perro.especie
+mi_Perro.edad
+mi_Perro.dueño
