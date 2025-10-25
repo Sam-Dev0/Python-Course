f5fff123fd3257adc066587c49afbbade5ab57d5
Samuel Molina <138609350+Sam-Dev0@users.noreply.github.com>
2025-10-25 09:44:50 -0500

    Refactor file handling and session logging

--- Leer archivos/Crear_Guardar_Archivo.py
+++ Leer archivos/Crear_Guardar_Archivo.py
@@ -1,7 +1,7 @@
 #mi_archivo =open('01_prueba.txt','r') # modo solo lectura
 #mi_archivo.write('soy otra linea') # presenta error, el archivo es modo solo  lectua
 
-"""
+
 mi_archivo =open('01_prueba.txt','a') # agrega una linea al final \n es para crear el salto de lina
 mi_archivo.write('\nsoy otra linea') 
 
@@ -10,7 +10,7 @@ mi_archivo.writelines(['\n','Hola ','como ','estas ','una linea mas larga'])
 mi_archivo_2 =open('02_prueba.txt','w') # reescribe el archivo utilizado, si no existe crea uno nuevo
 mi_archivo_2.write('Este es un texto')
 mi_archivo_2.write('\nEste es otro texto')
-"""
+
 
 registro_ultima_sesion =['Federico','20/12/2021','08:17;32 hs','Sin errores de carga']
 
@@ -22,4 +22,5 @@ ejercicio.close()
 
 ejercicio=open('registro.txt','r')
 print (ejercicio.read())
+
 ejercicio.close()
