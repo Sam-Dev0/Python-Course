6d735e0b0162e839df9c9f012b43718be059c5e0
Samuel Molina <138609350+Sam-Dev0@users.noreply.github.com>
2025-10-25 09:33:27 -0500

    Add Manada class to manage a pack of dogs

--- POO/Dunder.py
+++ POO/Dunder.py
@@ -17,3 +17,29 @@ class Perro:
 
 luna = Perro("Luna", 70)
 print(luna)
+
+class Manada:
+    def __init__(self,perros=None):
+        self._perros = list(perros) if perros else []
+    
+    #__len__ (manada)
+    def __len__(self):
+        return len(self._perros)
+    
+    #__iter__ recorre con for
+    def __iter__(self):
+        return iter(self._perros)
+    
+    #__getitem__ acceder por indice
+    def __getitem__(self,i):
+        return self._perros[i]
+    
+    #__contains__ perro in manada 
+    def __contains__(self,perro):
+        return any(p.nombre == perro.nombre for p in self._perros)
+    
+luna, rex = Perro('luna'), Perro('rex')
+m = Manada([luna, rex])
+print(len(m))
+for p in m: print(p.nombre)
+print(Perro('luna')in m)
