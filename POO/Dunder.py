8056df833a040b898ac9a4a9666fb8375a7fe561
Samuel Molina <138609350+Sam-Dev0@users.noreply.github.com>
2025-10-25 09:23:52 -0500

    Implement __str__ and __repr__ methods in Perro class
    Added string representation methods for the Perro class.

--- POO/Dunder.py
+++ POO/Dunder.py
@@ -1,1 +1,19 @@
+"""
+son "ganchos" que python llama automaticamente, para que tus clases se comporten
+bien al imprimir,comparar,iterar,sumar,usar,etc.
+"""
+class Perro:
+    #El perro (inicializa)
+    def __init__(self,nombre, energia = 60):
+        self.nombre = nombre
+        self.energia = energia
+    #Presentacion amigable (para Print)
+    def __str__(self):
+        return f'perro{self.nombre!r}, energia={self.energia}'
+    
+    #presentacion tecnica (para depurar)
+    def __repr__(self):
+        return f'Perro(nombre ={self.nombre!r}, energia={self.energia})'
 
+luna = Perro("Luna", 70)
+print(luna)
